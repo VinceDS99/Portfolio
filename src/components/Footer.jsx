3bce334bd3e09cb094bb73d486415907a42f8016
VinceDS99 <vincedos@hotmail.com>
2023-07-28 16:29:32 +0200

    Ajout lien Linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import github from '../assets/github.png'
 import {Link} from "react-router-dom"
 import mail from '../assets/mail.png'
 import phone from '../assets/phone.png'
+import Linkedin from '../assets/Linkedin.png'
 
 export default function Footer() 
 {
@@ -11,7 +12,7 @@ export default function Footer()
                 <div className="footer-links">
                     <div className="footer-circle">
 
-                        <Link to="https://github.com/VinceDS99?tab=repositories"  className='loc-text' target="_blank" title="Lien vers om GitHub">
+                        <Link to="https://github.com/VinceDS99?tab=repositories"  className='loc-text' target="_blank" title="Lien vers mon GitHub">
                         <img src={github} alt="Logo" className='logo-github' id='logo-github'/>
                         </Link>
 
@@ -22,10 +23,14 @@ export default function Footer()
                         <a href="tel:+33621037753" title='Mon numéro de téléphone : 06 21 03 77 53'>
                             <img src={phone} alt="Logo" className='logo-phone'/>
                         </a>
+
+                        <Link to="https://www.linkedin.com/in/vincent-dos-santos-863b98284/"  className='loc-text' target="_blank" title="Lien vers mon Linkedin">
+                            <img src={Linkedin} alt="Logo" className='logo-linkedin'/>
+                        </Link>
                     </div>
 
 
-                    <span className="footer-text">Mon lien github, mon mail et mon numéro</span>
+                    <span className="footer-text">Mon lien github, mon mail, mon numéro et mon linkedin</span>
                 </div>
 
 
